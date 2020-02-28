6315f11ecfd9406d9816f627015f7fbb32c79174
ilya <ilyamor@post.bgu.ac.il>
2020-02-28 21:11:35 +0200

    first commit2

---
+++ src/test/java/com/homeass/deduplication/DeduplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeass.deduplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeduplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
